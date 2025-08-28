016778e1c6ee123ab9c7acff0abbc074a0589b4e
Archit-KineticScape <archit@kineticscapestudios.com>
2025-08-28 14:15:12 +0530

    Update subscription.middleware.js

--- middlewares/subscription.middleware.js
+++ middlewares/subscription.middleware.js
@@ -1,11 +1,9 @@
 // middlewares/subscription.middleware.js
 const { checkAccess } = require("../services/subscription.service");
 
-async function requireSubscription(req, res, next) {
+module.exports = async function requireSubscription(req, res, next) {
   if (await checkAccess(req.user.id)) {
     return next();
   }
   return res.status(402).json({ error: "Subscription required." });
-}
-
-module.exports = { requireSubscription };
+};
